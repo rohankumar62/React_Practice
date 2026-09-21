0d5c4317cd85c33611e1c6cb1e9b8aa236b8e952
Rohan Kumar <rohankumarengineer@gmail.com>
2026-09-21 09:26:01 +0530

    Initial React practice project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Exemple01 from './Chapter-01/Part-01/Exemple01.jsx'
+
+function App() {
+  
+  return (
+   <>
+    <div>
+      <Exemple01 />
+    </div>
+   </>
+  )
+
+}
+
+export default App
---
+++ src/Chapter-01/Part-01/Exemple01.jsx
@@ -0,0 +1,20 @@
+function Exemple01() {
+  const user = {
+    name: "Rohan",
+    role: "Developer",
+  }
+
+  user.role = "React Developer"
+
+  console.log(user)
+
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>Name: {user.name}</p>
+      <p>Role: {user.role}</p>
+    </div>
+  )
+}
+
+export default Exemple01
